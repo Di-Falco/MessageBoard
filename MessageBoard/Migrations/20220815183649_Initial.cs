132a47cd58fe35bcaedd2545826cd7935b209ecf
Anthony DiFalco <aodifalco@gmail.com>
2022-08-15 13:29:47 -0700

    add crud methods
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

---
+++ MessageBoard/Migrations/20220815183649_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "varchar(20) CHARACTER SET utf8mb4", maxLength: 20, nullable: false),
+                    Topic = table.Column<string>(type: "varchar(60) CHARACTER SET utf8mb4", maxLength: 60, nullable: false),
+                    MessageBody = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    PostDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
---
+++ MessageBoard/Models/Message.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Globalization;
+
+namespace MessageBoard.Models
+{
+  public class Message
+  {
+    public int MessageId { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string Username { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Topic must be less than 50 characters.")]
+    public string Topic { get; set; }
+    [Required]
+    public string MessageBody { get; set; }
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "0:MM/dd/yyy")]
+    public DateTime PostDate { get; set; }
+  }
+}
