13071de50e05d7befc98b405aaddf28aebcb0d8e
Anthony DiFalco <aodifalco@gmail.com>
2022-08-16 16:57:11 -0700

    add accountscontroller

---
+++ MessageClient/Controllers/AccountsController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using MessageClient.ViewModels;
+using MessageClient.Models;
+
+namespace MessageClient.Controllers
+{
+  public class AccountsController : Controller
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.Title="Account";
+      ViewBag.Subtitle="Home";
+      return View();
+    }
+
+    public IActionResult Register()
+    {
+      ViewBag.Title="Account";
+      ViewBag.Subtitle="Register";
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      var user = new ApplicationUser { Name = model.Name, Email = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    public ActionResult Login()
+    {
+      ViewBag.Title="Account";
+      ViewBag.Subtitle="Log In";
+      return View();
+    }
+
+    [HttpPost]
+    public Async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Name, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
+
+  }
+}
